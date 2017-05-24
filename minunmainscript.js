12dd158a13797f604569229baa96f07712c0b53f
ENivala <izari-kiero@hotmail.com>
2017-05-24 15:53:42 +0300

    Eka uppi.
    ..

---
+++ minunmainscript.js
@@ -0,0 +1,22 @@
+
+	function munFunktio()
+	{
+		document.getElementById("kuva").src ="trol.jpg";
+		document.getElementById("kuva").style.visibility ="visible";
+		
+		
+	}
+	
+	
+	
+	function toinenFunktio()
+	{
+		document.getElementById("kuva").style.width="100%";
+		document.getElementById("kuva").style.height="100%";
+		
+		
+	}
+	
+	
+	
+   
